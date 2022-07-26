e9207dd320ddb68169e4d706744510e8cf902ee0
Abiola Akinnubi <abiola.akinnubi@activision.com>
2022-07-26 10:02:29 -0500

    Learning progress on level editor. couple of bugs to be fixed.

--- LevelEditor/LevelEditor.cpp
+++ LevelEditor/LevelEditor.cpp
@@ -2,9 +2,38 @@
 //
 
 #include <iostream>
+#include <conio.h>
 using namespace std;
+constexpr char kCursor = '_';
 
+//Borders
+constexpr char kTopRightBorder = 187;
+constexpr char kTopLeftBorder = 201;
+constexpr char kBottomRightBorder = 188;
+constexpr char kBottomLeftBorder = 200;
+constexpr char kHorizontalBorder = 205;
+constexpr char kVerticalBorder = 186;
+
+//Arrows
+constexpr int kArrowInput = 224;
+constexpr int kLeftArrow = 75;
+constexpr int kUpArrow = 72;
+constexpr int kDownArrow = 80;
+constexpr int kRightArrow = 77;
+
+//
+constexpr int kEsacpe = 27;
+
+
+//Functions
 void GetLevelDimensions(int& width, int& height);
+void DisplayLevel(char* pLevel, int width, int height, int cursorX, int cursorY);
+int GetIndexFromXY(int x, int y, int width);
+void DisplayTopBorder(int width);
+void DisplayBottomBorder(int width);
+void DisplayLeftBorder();
+void DisplayRightBorder();
+bool EditLevel(char* pLevel, int& cursorX, int& cursorY, int width, int height);
 int main()
 {
 	int levelWidth;
@@ -14,6 +43,16 @@ int main()
 	for (int i = 0; i < levelWidth * levelHeight; i++) {
 		pLevel[i] = ' ';
 	}
+	int cursorX = 0;
+	int cursorY = 0;
+	bool doneEditing = false;
+	 while (!doneEditing) {
+		 system("cls");
+		 DisplayLevel(pLevel, levelWidth, levelHeight, cursorX, cursorY);
+		 doneEditing = EditLevel(pLevel, cursorX, cursorY, levelWidth, levelHeight);
+	}
+	 system("cls");
+	 DisplayLevel(pLevel, levelWidth, levelHeight,-1,-1);
 	delete[] pLevel;
 	pLevel = nullptr;
 }
@@ -24,4 +63,101 @@ void GetLevelDimensions(int& width, int& height)
 	cin >> width;
 	cout << "Enter the height of your level: ";
 	cin >> height;
+}
+
+void DisplayLevel(char* pLevel, int width, int height, int cursorX,int cursorY)
+{
+	DisplayTopBorder(width);
+
+	for (int y = 0; y < height; y++) {
+		DisplayLeftBorder();
+		for (int x = 0; x < width; x++) {
+			int index = GetIndexFromXY(x, y, width);
+			cout << pLevel[index];
+		}
+		DisplayRightBorder();
+	}
+	DisplayBottomBorder(width);
+}
+
+int GetIndexFromXY(int x, int y, int width)
+{
+	return x + y * width;
+}
+
+void DisplayTopBorder(int width)
+{
+	cout << kTopLeftBorder;
+	for (int i = 0; i < width; i++) {
+		cout << kHorizontalBorder;
+	}
+	cout << kTopRightBorder << endl;
+}
+
+void DisplayBottomBorder(int width)
+{
+	cout << kBottomLeftBorder;
+	for (int i = 0; i < width; i++) {
+		cout << kHorizontalBorder;
+	}
+	cout << kBottomRightBorder << endl;
+}
+
+void DisplayLeftBorder()
+{
+	cout << kVerticalBorder;
+}
+
+void DisplayRightBorder()
+{
+	cout << kVerticalBorder << endl;
+}
+
+bool EditLevel(char* pLevel, int& cursorX, int& cursorY, int width, int height)
+{
+	int newCursorX = cursorX;
+	int newCursorY = cursorY;
+	int intInput = _getch();
+	if (intInput == kArrowInput) {
+		int arrowInput = _getch();
+		switch (arrowInput)
+		{
+		case kLeftArrow:
+			newCursorX--;
+			break;
+		case kRightArrow:
+			newCursorX++;
+			break;
+		case kUpArrow:
+			newCursorY--;
+			break;
+		case kDownArrow:
+			newCursorY++;
+			break;
+		}
+		if (newCursorX < 0) {
+			newCursorX = 0;
+		}
+		else if (newCursorX == width) {
+			newCursorX = width - 1;
+		}
+		if (newCursorY < 0) {
+			newCursorY = 0;
+		}
+		else if (newCursorY == width) {
+			newCursorY = width - 1;
+		}
+		cursorX = newCursorX;
+		cursorY = newCursorY;
+	}
+	else {
+		if (intInput == kEsacpe) {
+			return true;
+		}
+		else {
+			int index = GetIndexFromXY(newCursorX, newCursorY, width);
+			pLevel[index] = (char)intInput;
+		}
+	}
+	return false;
 }
