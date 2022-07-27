a6ca9aa6f98d62190324d755336bf0841d19b4eb
Abiola Akinnubi <abiola.akinnubi@activision.com>
2022-07-26 20:16:22 -0500

    Color Attribute configured for the Maze program

--- MazeGameLearning.cpp
+++ MazeGameLearning.cpp
@@ -3,30 +3,42 @@
 
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
 using namespace std;
 int GetIndexFromCoordinates(int x, int y, int width);
-void DrawLevel(char level[], int width, int height, int playerX, int playerY);
+void DrawLevel(char level[], int width, int height, int playerX, int playerY, bool playerHasKey);
 bool UpdatePlayerPosition(char level[], int& playerX, int& playerY, int width,bool& playerHasKey);
+void PlayDoorClosedSound();
+void PlayDoorOpenSound();
+void PlayKeyPickupSound();
+void PlayWinSound();
 constexpr char kPlayerSymbol = '@';
+constexpr char WAL = 219;
+constexpr char KEY = 232;
+constexpr char DOR = 179;
+const char GOL = 36;
+constexpr int kOpenDoorColor = 10;
+constexpr int kClosedDoorColor = 12;
+constexpr int kRegularColor = 7;
 int main()
 {
 	constexpr int kwidth = 25;
 	constexpr int kheight = 15;
-	char levelArray[]{  '+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ','*','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+','-',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+','-','-','-','-','-','-','-','-','-','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','D',' ',' ',' ',' ',' ',' ',' ',' ','X','|',
-						'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+'
+	char levelArray[]{  WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',KEY,WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,WAL,' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',WAL,
+						WAL,' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',DOR,' ',' ',' ',' ',' ',' ',' ',' ',GOL,WAL,
+						WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL,WAL
 };
 	int playerX = 1;
 	int playerY = 1;
@@ -34,9 +46,13 @@ int main()
 	bool gameOver = false;
 	while (!gameOver) {
 		system("cls");
-		DrawLevel(levelArray, kwidth, kheight, playerX, playerY);
+		DrawLevel(levelArray, kwidth, kheight, playerX, playerY,playerHasKey);
 		gameOver = UpdatePlayerPosition(levelArray,playerX, playerY,kwidth, playerHasKey);
 	}
+	system("cls");
+	DrawLevel(levelArray, kwidth, kheight, playerX, playerY,playerHasKey);
+	cout << "YOU WIN!!!!!!!" << endl;
+	PlayWinSound();
 
 }
 
@@ -46,7 +62,7 @@ int GetIndexFromCoordinates(int x, int y, int width)
 	return x+y*width;
 }
 
-void DrawLevel(char level[], int width, int height, int playerX, int playerY)
+void DrawLevel(char level[], int width, int height, int playerX, int playerY, bool playerHasKey)
 {
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
@@ -54,8 +70,20 @@ void DrawLevel(char level[], int width, int height, int playerX, int playerY)
 				cout << kPlayerSymbol;
 			}
 			else {
-				int index = GetIndexFromCoordinates(x, y, width);
-				cout << level[index];
+				int indexToPrint = GetIndexFromCoordinates(x, y, width);
+				HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+				if (level[indexToPrint] == DOR) {
+					if (playerHasKey) {
+						SetConsoleTextAttribute(console, kOpenDoorColor);
+					}
+					else {
+						SetConsoleTextAttribute(console, kClosedDoorColor);
+					}
+				}
+				else {
+					SetConsoleTextAttribute(console, kRegularColor);
+				}
+				cout << level[indexToPrint];
 			}
 
 		}
@@ -103,23 +131,58 @@ bool UpdatePlayerPosition(char level[], int& playerX, int& playerY, int width,bo
 		playerX = newPlayerX;
 		playerY = newPlayerY;
 	}
-	else if (level[index] == '*') {
+	else if (level[index] == KEY) {
 		playerHasKey = true;
 		level[index] = ' ';
 		playerX = newPlayerX;
 		playerY = newPlayerY;
 	}
-	else if (level[index] == 'D' && playerHasKey) {
+	else if (level[index] == DOR && playerHasKey) {
 		level[index] = ' ';
 		playerHasKey = false;
 		playerX = newPlayerX;
 		playerY = newPlayerY;
+		PlayDoorOpenSound();
 	}
-	else if (level[index] == 'X') {
+	else if (level[index] == DOR && !playerHasKey) {
+		/*level[index] = ' ';
+		playerHasKey = false;
+		playerX = newPlayerX;
+		playerY = newPlayerY;*/
+		PlayDoorClosedSound();
+	}
+	else if (level[index] == GOL) {
 		level[index] = ' ';
 		playerX = newPlayerX;
 		playerY = newPlayerY;
 		return true;
 	}
 	return false;
+}
+void PlayDoorClosedSound()
+{
+	Beep(500, 75);
+	Beep(500, 75);
+}
+void PlayDoorOpenSound()
+{
+	Beep(1397, 200);
+}
+
+void PlayKeyPickupSound()
+{
+	Beep(1568, 200);
+}
+
+void PlayWinSound()
+{
+	Beep(1568, 200);
+	Beep(1568, 200);
+	Beep(1568, 200);
+	Beep(1245, 200);
+	Beep(1397, 200);
+	Beep(1397, 200);
+	Beep(1397, 200);
+	Beep(1175, 1000);
+
 }
